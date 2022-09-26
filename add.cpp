821bfc3c800ce2636b361d03323ded9d026f1c7a
ANURAG SINGH <103880648+devilanurag@users.noreply.github.com>
2022-09-26 22:45:16 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,13 @@
+//wap to take input from user and add them....
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,add;
+    cout<<"enter the first no.:";
+    cin>>n1;
+    cout<<"enter the second no.:";
+    cin>>n2;
+    add=n1+n2;
+    cout<<"the result of two no.s is:"<<add;
+}
