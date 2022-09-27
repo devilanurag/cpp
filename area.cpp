0a43a0d969c88b70561789645f973e967890e3f0
ANURAG SINGH <103880648+devilanurag@users.noreply.github.com>
2022-09-27 10:45:56 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,12 @@
+//wap to take input as a radius and print the area of circle
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,area;
+    cout<<"enter the radius of circle:";
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"the area of circle is :"<<area;
+    return 0;
+}
